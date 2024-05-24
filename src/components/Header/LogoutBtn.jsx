8108308fe0f0939beb962de4f0d75e9f435be3c2
Dipen Kumar <dipenbachani@gmail.com>
2024-05-23 23:08:07 -0600

    error handle

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -17,7 +17,10 @@ function LogoutBtn() {
 	};
 
 	return (
-		<button className="inline-block px-6 py-2 duration-150 hover:bg-blue-100 rounded-full">
+		<button
+			className="inline-block px-6 py-2 duration-150 hover:bg-blue-100 rounded-full"
+			onClick={logoutHandler}
+		>
 			Logout
 		</button>
 	);
