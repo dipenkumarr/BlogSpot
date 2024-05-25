6998e8a6e3fa1f4acec89efa4c52d3abc67a33f0
Dipen Kumar <dipenbachani@gmail.com>
2024-05-25 02:59:58 -0600

    api change name

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
 	appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 	appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
 	appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-	tinymcekey: String(import.meta.env.REACT_APP_TINYMCE_API_KEY),
+	tinymcekey: String(import.meta.env.REACT_APP_TINYMCE_API),
 };
 
 export default conf;
